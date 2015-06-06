dd37e5b3b8262d440938b5b2613ebb47771ac687
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-06-06 14:15:04 -0700

    Skip header line.

--- src/VipFormat/candidate.py
+++ src/VipFormat/candidate.py
@@ -5,7 +5,7 @@ import common
 
 
 def main():
-    base_name = "precincts_20140321.csv"
+    base_name = "candidates.csv"
     for line in common.csv_lines(base_name):
         print(line)
 
--- src/VipFormat/common.py
+++ src/VipFormat/common.py
@@ -15,5 +15,7 @@ def csv_lines(base_name):
     path = os.path.join('../../data', base_name)
     with open(path, 'rb') as csvfile:
         csvreader = csv.reader(csvfile)
+        # Skip header line.
+        next(csvreader)
         for line in csvreader:
             yield line
