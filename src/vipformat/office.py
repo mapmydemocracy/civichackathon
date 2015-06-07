c47b17a5be24499bffd86237f397d95c01687096
Ed Felten <felten@cs.princeton.edu>
2015-06-06 19:39:55 -0700

    Update/fix code to emit offices and contests

--- src/vipformat/office.py
+++ src/vipformat/office.py
@@ -1,19 +1,39 @@
+
 import csv
+
 from vipformat import common, electoralDistrict
+
+officeCsvFile = '../../data/offices.csv'
 
 def emitOffice(line):
     object_id = line[0]
     d = [
-        ('OfficeHolderPersonId', line[1]), 
-        ('Name', line[5])]
+        ('ElectoralDistrictID', line[5]),
+        ('Name', '<Text language="en">'+line[1]+'</Text>'),
+    ]
     return common.pairlistToXml('Office', d, object_id=object_id)
-    
+
+def emitContest(line):
+    object_id = line[0]
+    d = [
+        ('BallotTitle', '<Text language="en">'+line[1]+'</Text>'),
+        ('ElectoralDistrictID', line[5]),
+        ('NumberElected', '1'),
+        ('VotesAllowed', '1'),
+        ('OfficeID', line[0])
+    ]
+    return common.pairlistToXml('CandidateContest', d, object_id=object_id)
+
 def emitAllOffices():
     ret = ''
-    base_name = "offices.csv"
+    base_name = 'offices.csv'
     for line in common.csv_lines(base_name):
         ret += emitOffice(line)
+        ret += emitContest(line)
+        
     return ret
 
 if __name__=='__main__':
     print emitAllOffices()
+
+
