cb464939202282b0a3d2bb50a72a5275fab440f2
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-06-06 22:42:57 -0700

    Add State object.

--- src/vipformat/emitAllXml.py
+++ src/vipformat/emitAllXml.py
@@ -1,5 +1,6 @@
 
-from vipformat import candidate, electoralDistrict, precinct, office, verbatim
+from vipformat.common import indent
+from vipformat import candidate, common, electoralDistrict, office, precinct, verbatim
 
 
 def emitProlog():
@@ -8,8 +9,29 @@ def emitProlog():
 def emitEpilog():
     return '</VipObject>\n'
 
+def make_states():
+    """
+    <xs:complexType name="State">
+        <xs:sequence>
+          <xs:element name="ElectionAdministrationId" type="xs:IDREF" minOccurs="0" />
+          <xs:element name="ExternalIdentifiers" type="ExternalIdentifiers" minOccurs="0" />
+          <xs:element name="Name" type="xs:string" />
+          <xs:element name="PollingLocationId" type="xs:IDREF" minOccurs="0" maxOccurs="unbounded" />
+        </xs:sequence>
+        <xs:attribute name="id" type="xs:ID" use="required" />
+      </xs:complexType>
+    """
+    state_id = 'state_ca'
+    d = [
+        ('Name', 'California'),
+    ]
+    xml = common.pairlistToXml('State', d, object_id=state_id)
+
+    return xml
+
 def emitAll():
     ret = emitProlog()
+    ret += indent(make_states())
     ret += precinct.emitAllPrecincts()
     ret += electoralDistrict.emitAllElectoralDistricts()
     ret += candidate.parse_candidates()
