6383f230b5a7c0cd25b55b302bb8c95d98137aaf
erispoe <favrebulle@gmail.com>
2015-06-06 20:18:07 -0700

    Directified the input form

---
+++ client/client/components/chForm/chForm.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('clientApp')
+  .directive('chForm', function () {
+    return {
+      templateUrl: 'components/chForm/chForm.html',
+      restrict: 'EA',
+      scope: {
+      	items: '=',
+      	title: '@'
+      },
+      controller: 'chFormCtrl'
+    };
+  })
+
+.controller("chFormCtrl", ['$scope', '$state', '$http', 
+	function($scope, $state, $http){
+
+	}])
+
+  ;
