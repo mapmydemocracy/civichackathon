66cccf9f92e1fd467dfd7ce62a34efb874e32849
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-06-08 06:23:38 -0700

    Add ElectionsUri.

--- src/vipformat/election_admin.py
+++ src/vipformat/election_admin.py
@@ -1,51 +1,4 @@
-"""
-Supports creating the ElectionsAdministration object:
-
-<xs:complexType name="ElectionAdministration">
-  <xs:sequence>
-    <xs:element name="AbsenteeUri" type="xs:anyURI" minOccurs="0" />
-    <xs:element name="AmIRegisteredUri" type="xs:anyURI" minOccurs="0" />
-    <!-- A locality may have more than one department with each handling different services. -->
-    <xs:element name="Department" maxOccurs="unbounded">
-      <xs:complexType>
-        <xs:sequence>
-          <xs:element name="ContactInformation" type="ContactInformation" minOccurs="0" />
-          <xs:element name="ElectionOfficialPersonId" type="xs:IDREF" minOccurs="0" />
-          <xs:element name="VoterService" minOccurs="0" maxOccurs="unbounded">
-            <xs:complexType>
-              <xs:all>
-                <!--
-                    The contact information below can be used to override or
-                    add specific fields to the base Departmental contact information if
-                    the service has different information.  For example, if the voter
-                    service has its own phone number, the ContactInformation object
-                    below can be an object containing only a Phone element.
-                  -->
-                <xs:element name="ContactInformation" type="ContactInformation" minOccurs="0" />
-                <xs:element name="Description" type="InternationalizedText" minOccurs="0" />
-                <!--
-                    This is for use if a certain person handles the particular service,
-                    for example a contact person for overseas voting.
-                  -->
-                <xs:element name="ElectionOfficialPersonId" type="xs:IDREF" minOccurs="0" />
-                <xs:element name="Type" type="VoterServiceType" minOccurs="0" />
-                <xs:element name="OtherType" type="xs:string" minOccurs="0" />
-              </xs:all>
-            </xs:complexType>
-          </xs:element>
-        </xs:sequence>
-      </xs:complexType>
-    </xs:element>
-    <xs:element name="ElectionsUri" type="xs:anyURI" minOccurs="0" />
-    <xs:element name="RegistrationUri" type="xs:anyURI" minOccurs="0" />
-    <xs:element name="RulesUri" type="xs:anyURI" minOccurs="0" />
-    <xs:element name="WhatIsOnMyBallotUri" type="xs:anyURI" minOccurs="0" />
-    <xs:element name="WhereDoIVoteUri" type="xs:anyURI" minOccurs="0" />
-  </xs:sequence>
-  <xs:attribute name="id" type="xs:ID" use="required" />
-</xs:complexType>
-"""
-
+"""Supports creating the ElectionsAdministration object."""
 
 import csv
 from vipformat import common
@@ -107,11 +60,28 @@ def make_department_xml():
 
 def make_election_administration_xml():
     """
-    Fields:
-        Department, ContactInformation, Address, Phone, Fax, TTY
+    <xs:complexType name="ElectionAdministration">
+      <xs:sequence>
+        <xs:element name="AbsenteeUri" type="xs:anyURI" minOccurs="0" />
+        <xs:element name="AmIRegisteredUri" type="xs:anyURI" minOccurs="0" />
+        <!-- A locality may have more than one department with each handling different services. -->
+        <xs:element name="Department" maxOccurs="unbounded">
+        ...
+        </xs:element>
+        <xs:element name="ElectionsUri" type="xs:anyURI" minOccurs="0" />
+        <xs:element name="RegistrationUri" type="xs:anyURI" minOccurs="0" />
+        <xs:element name="RulesUri" type="xs:anyURI" minOccurs="0" />
+        <xs:element name="WhatIsOnMyBallotUri" type="xs:anyURI" minOccurs="0" />
+        <xs:element name="WhereDoIVoteUri" type="xs:anyURI" minOccurs="0" />
+      </xs:sequence>
+      <xs:attribute name="id" type="xs:ID" use="required" />
+    </xs:complexType>
     """
     department_xml = make_department_xml()
-    d = [department_xml]
+    d = [
+        department_xml,
+        ("ElectionsUri", "http://sfelections.org"),
+    ]
     xml = pairlistToXml('ElectionAdministration', d, object_id=SF_ELECTION_ADMIN_ID)
 
     return xml
